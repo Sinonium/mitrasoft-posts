8abec3c300bdbc2e4af233cf9c0e79738fec441d
sinonium <sinoniumx@gmail.com>
2023-05-22 18:21:07 +0600

    base structure

---
+++ src/pages/About/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './index.scss'
+
+export const About = () => {
+  return (
+    <div>About</div>
+  )
+}
---
+++ src/pages/Home/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './index.scss'
+
+
+export const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
