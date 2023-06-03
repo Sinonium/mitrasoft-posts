bd2ad6d9d17c3c14e49413065b8a686c56f1c78f
sinonium <sinoniumx@gmail.com>
2023-06-04 01:33:50 +0600

    added routing, prepared pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import { Route, Routes } from 'react-router-dom';
-import { About, Details, Home } from './pages';
-import './App.css';
+import { Container } from 'react-bootstrap';
+
+import NavbarComponent from './components/Navbar';
+import Home from './pages/Home/Home';
+import Details from './pages/Details/Details';
+import About from './pages/About/About';
+import './App.scss';
+
 
 function App() {
 	return (
 		<div className='App'>
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/about' element={<About />} />
-				<Route path='/about' element={<Details />} />
-			</Routes>
+			<Container>
+				<NavbarComponent />
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route
+						path='/about'
+						element={<About />}
+					/>
+					<Route path='/details/:id' element={<Details />} />
+				</Routes>
+			</Container>
 		</div>
 	);
 }
