3b2c21ccdc77d5ef60eeceefea8921ea01ffae6b
sinonium <sinoniumx@gmail.com>
2023-06-07 02:41:21 +0600

    removed devtools extension from prod

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,10 @@
-import { applyMiddleware, compose, legacy_createStore as createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import createSagaMiddleware from 'redux-saga'
-import rootReducer from "./reducers/rootReducer";
-import { rootWatcher } from "../saga";
+import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from './reducers/rootReducer';
+import { rootWatcher } from '../saga';
 
-const sagaMiddleware = createSagaMiddleware()
+const sagaMiddleware = createSagaMiddleware();
 
-export const store = createStore(
-  rootReducer,
-  compose(
-    composeWithDevTools(applyMiddleware(sagaMiddleware))
-  ),
-);
+export const store = createStore(rootReducer, compose(applyMiddleware(sagaMiddleware)));
 
-sagaMiddleware.run(rootWatcher)
+sagaMiddleware.run(rootWatcher);
